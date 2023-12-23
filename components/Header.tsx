4cb1da13030b3adaeba155edb969ad72f6e88255
Harsh Pandey <pandeyharsh704@gmail.com>
2023-12-23 13:41:45 +0530

    header route url corrected

--- components/Header.tsx
+++ components/Header.tsx
@@ -78,7 +78,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                 Logout
               </Button>
               <Button
-                onClick={() => router.push("/account")}
+                onClick={() => router.push("/")}
                 className="bg-white"
               >
                 <FaUserAlt />
