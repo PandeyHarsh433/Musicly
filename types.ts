3e1aa96c8cf7bf39ca6a41dfa0c1c92711f00bcd
Harsh Pandey <pandeyharsh704@gmail.com>
2023-12-23 12:22:38 +0530

    first commit

---
+++ types.ts
@@ -0,0 +1,21 @@
+export interface Song {
+  id: string;
+  user_id: string;
+  author: string;
+  title: string;
+  song_path: string;
+  image_path: string;
+}
+
+export interface Product {
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+}
+
+export interface Customer {
+  id: string;
+  stripe_customer_id?: string;
+}
